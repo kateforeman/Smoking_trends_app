699ab5db17145543ba82ecf5cb0cf9d9b57719da
Jonathan Brown <jonny.brown1812@gmail.com>
2020-12-08 16:03:23 +0000

    push changes to life satisfaction ggplot

--- ggplots/life_expectancy_plot.R
+++ ggplots/life_expectancy_plot.R
@@ -2,7 +2,7 @@ source("cleaning_scripts/life_expectancy_cleaning.R")
 
 life_expectancy_plot <- life_expectancy_cleaned %>% 
   ggplot(aes(x = date_code, y = count_years, colour = sex)) +
-  geom_point() +
+  geom_point()+
   geom_line()+
   labs(x = "Year", y = "Count Years", title = "Life Expectancy") +
   theme_light()
