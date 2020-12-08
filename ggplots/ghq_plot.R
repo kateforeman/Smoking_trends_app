619e5681afc7677625ec835aa5f28959441c422a
Jonathan Brown <jonny.brown1812@gmail.com>
2020-12-08 09:52:29 +0000

    clean up ggplots

--- ggplots/ghq_plot.R
+++ ggplots/ghq_plot.R
@@ -16,12 +16,13 @@ scotland_survey_cleaned %>%
                                                    "General health questionnaire (GHQ-12): Score 4+" = "4+")) %>%
  
   ggplot() +
+  ggtitle("General Health Questionnaire")+
   aes(x = date_code, y = percent, colour = scottish_health_survey_indicator) +
   geom_line() +
   scale_x_continuous(breaks = c(2008, 2009, 2010, 2011, 2012, 2013,
                                 2014, 2015, 2016, 2017, 2018, 2019), limits = c(2010, 2019)) +
   geom_ribbon(aes(ymin=lower_confidence_limit_95_percent,
-                  ymax=upper_confidence_limit_95_percent), linetype=2, alpha=0.1) +
+                  ymax=upper_confidence_limit_95_percent), linetype=3, alpha=0.1) +
   labs(colour = "GHQ Score",
        x = "Year",
        y = "Percentage of Adults") +
---
+++ ggplots/life_satisfaction.R
@@ -0,0 +1,25 @@
+
+library(tidyverse) 
+library(janitor) 
+library(ggplot2)
+
+
+
+
+life_satisfaction <- scotland_survey_cleaned %>% 
+  select(date_code, scottish_health_survey_indicator, sex, percent, lower_confidence_limit_95_percent, upper_confidence_limit_95_percent) %>% 
+  filter(scottish_health_survey_indicator ==  c("Life satisfaction: Above the mode (9 to 10-Extremely satisfied)", "Life satisfaction: Below the mode (0-Extremely dissatisfied to 7)","Life satisfaction: Mode (8)")) 
+
+life_satisfaction
+  
+
+life_satisfaction %>% 
+  mutate(column_name = recode(column_name, "Life satisfaction: Above the mode (9 to 10-Extremely satisfied" = "Above Mode")) %>% 
+  filter(sex == "All") %>% 
+  ggplot(aes(date_code, percent, colour = scottish_health_survey_indicator)) + 
+  geom_line() + 
+  geom_point()+
+  scale_x_continuous(breaks = c(2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019))+
+  labs(x = "Year", y = "Percentage", title = "Life Satisfaction", colour = "Life satisfaction Indicator") +
+  #CHANGE LEGEND NAMES? scale_fill_discret?
+  theme_light()
