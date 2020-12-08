97a1ba69d8caa94d16cd5bf7f247174be560479b
Peter.Menzies <menziesprf@gmail.com>
2020-12-08 23:38:12 +0000

    working map not in app

--- maps/smoking_map.R
+++ maps/smoking_map.R
@@ -1,16 +1,9 @@
-library(tidyverse)
-library(sf)
-library(rgdal)
-library(rmapshaper)
 
-chunks <- readOGR(dsn = "data/SG_NHS_HealthBoards_2019", layer = "SG_NHS_HealthBoards_2019")
-
-good_chunks <- st_as_sf(chunks)
 
 
 class(good_chunks)
 
-gooder_chunks <- ms_simplify(good_chunks)
+
 
 class(gooder_chunks)
 
@@ -18,7 +11,7 @@ plot(gooder_chunks)
 
 
 gooder_chunks %>% 
-  select(HBCode, HBName, geometry) %>% 
+   %>% 
   plot()
 
 
